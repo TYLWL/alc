10ddab8ed5de4c1b8fc9d4f068d3792bdcf905cb
shouwei <shouwei.tang@oatservice.com>
2020-06-02 11:19:14 +0800

    changed container and context

--- alc/src/main/java/com/OAT/Routing/DataAccess/DBReader.java
+++ alc/src/main/java/com/OAT/Routing/DataAccess/DBReader.java
@@ -1,20 +1,33 @@
 package com.OAT.Routing.DataAccess;
 
 import com.OAT.Routing.DataEntity.*;
+
 import com.OAT.Routing.sql.SqlConnection;
 
 import java.sql.ResultSet;
 
 public class DBReader {
+	/*
     private static SqlConnection sqlConnection;
     private static DataContainer dataContainer;
-
+*/
+	private static DataContext _dataContext;
+	
+	
+    public DBReader(DataContext dataContext)
+    {
+    	//sqlConnection = dataContext.getSqlConnection();
+    	//dataContainer = dataContainer.getDataContainer();
+    	
+    	_dataContext =dataContext;
+    }
+    
     //product
     public void setProducts()throws Exception{
-        ResultSet products = sqlConnection.getObject("select * from product");
+        ResultSet products = _dataContext.getSqlConnection().getObject("select * from product");
         while (products.next()){
             OProduct product = new OProduct(products.getString("ProductID"),products.getString("Description"));
-            dataContainer.addProduct(product);
+            _dataContext.getDataContainer().addProduct(product);
         }
     }
     //dailySource
@@ -22,40 +35,40 @@ public class DBReader {
 
     //customer
     public void setCustomers() throws Exception{
-        ResultSet customers = sqlConnection.getObject("select * from master_customer");
+        ResultSet customers = _dataContext.getSqlConnection().getObject("select * from master_customer");
         while (customers.next()){
             OCustomer customer = new OCustomer(customers.getString("RegionID"), customers.getDouble("Latitude"),customers.getDouble("Longitude"),customers.getString("CustomerID"),customers.getString("Description"));
-            dataContainer.addCustomer(customer);
+            _dataContext.getDataContainer().addCustomer(customer);
         }
     }
 
     //depot
     public void setoDepots()throws Exception {
-        ResultSet depots = sqlConnection.getObject("select * from master_depot");
+        ResultSet depots = _dataContext.getSqlConnection().getObject("select * from master_depot");
         while (depots.next()){
             ODepot depot = new ODepot(depots.getString("RegionID"), depots.getDouble("Latitude"),depots.getDouble("Longitude"),depots.getString("DepotID"),depots.getInt("MaxTrailers"),depots.getString("Description"));
-            dataContainer.addDepot(depot);
+            _dataContext.getDataContainer().addDepot(depot);
         }
     }
 
 
     //ODistrict
     public void setDistricts()throws Exception {
-        ResultSet districts = sqlConnection.getObject("select * from master_district");
+        ResultSet districts = _dataContext.getSqlConnection().getObject("select * from master_district");
             while (districts.next()){
             ODistrict district = new ODistrict(districts.getString("RegionID"), districts.getString("DistrictID"),districts.getString("Prefecture"),districts.getString("County"),districts.getString("ERStart"),districts.getString("EREnd"),districts.getString("NMStart"),districts.getString("NMEnd"),districts.getBoolean("HasHolidayRestriction"),districts.getString("PermittedTrailersID").split(","),districts.getString("Description"));
-            dataContainer.addDistrict(district);
+            _dataContext.getDataContainer().addDistrict(district);
         }
     }
 
     //ODriver
     public void setDrivers()throws Exception {
-        ResultSet drivers = sqlConnection.getObject("select * from master_driver");
+        ResultSet drivers = _dataContext.getSqlConnection().getObject("select * from master_driver");
         while (drivers.next()){
-            ResultSet depot = sqlConnection.getObject("select RegionID from master_depot where DepotID = " + drivers.getString("DepotID"));
+            ResultSet depot = _dataContext.getSqlConnection().getObject("select RegionID from master_depot where DepotID = " + drivers.getString("DepotID"));
             depot.next();
             ODriver driver = new ODriver(depot.getString("RegionID"),0,0,drivers.getString("DepotID"), drivers.getString("PersonnelID"),drivers.getString("Name"),drivers.getString("DedicatedTrailerID"),drivers.getString("DedicatedProductID"),drivers.getBoolean("IsMeddicalLoxCertified"),drivers.getBoolean("IsOnlyRigid"),drivers.getString("Description"));
-            dataContainer.addDriver(driver);
+            _dataContext.getDataContainer().addDriver(driver);
         }
     }
 
@@ -114,36 +127,36 @@ public class DBReader {
 
     //ORegion
     public void setRegions() throws Exception{
-        ResultSet regions = sqlConnection.getObject("select * from region");
+        ResultSet regions = _dataContext.getSqlConnection().getObject("select * from region");
         while (regions.next()){
             ORegion region = new ORegion(regions.getString("RegionID"), regions.getString("Description")) {};
-            dataContainer.addRegion(region);
+            _dataContext.getDataContainer().addRegion(region);
         }
     }
 
     //OSource
     public void setSources() throws Exception{
-        ResultSet sources = sqlConnection.getObject("select * from master_source");
+        ResultSet sources = _dataContext.getSqlConnection().getObject("select * from master_source");
         System.out.println(sources);
-        ResultSet sourceAttr = sqlConnection.getObject("select * from sourceattribute");
+        ResultSet sourceAttr = _dataContext.getSqlConnection().getObject("select * from sourceattribute");
         while (sources.next()){
             OSource source = new OSource(sources.getString("RegionID"),sources.getDouble("Latitude"),sources.getDouble("Longitude"), sources.getString("Description")) {};
             sources.close();
             while (sourceAttr.next()){
                 source.addSourceAttribute(sourceAttr.getString("SourceAttrID"));
             }
-            dataContainer.addSource(source);
+            _dataContext.getDataContainer().addSource(source);
         }
     }
 
 
 
-
+/*
     public DataContainer readData()throws Exception{
         setProducts();
         return dataContainer;
     }
     public DBReader()throws Exception{
         sqlConnection = new SqlConnection();
-    }
+    }*/
 }
--- alc/src/main/java/com/OAT/Routing/DataAccess/DataContext.java
+++ alc/src/main/java/com/OAT/Routing/DataAccess/DataContext.java
@@ -1,11 +1,39 @@
 package com.OAT.Routing.DataAccess;
 
+import com.OAT.Routing.sql.SqlConnection;
+
 public class DataContext {
-    private static DataContainer dataContainer;
+    
+	private static DataContainer dataContainer;
+    private static SqlConnection sqlConnection;
+    
     public DataContext(){
+    	
+    	try
+    	{
+    	   dataContainer =  new DataContainer();
+    	   sqlConnection = new SqlConnection();
+    	   createDataContainer();
+    	   
+    	}catch (Exception e)
+    	{
+    		e.printStackTrace();
+    	}
+    	
     }
     public void createDataContainer()throws Exception{
-        DBReader dbReader = new DBReader();
-        dataContainer = dbReader.readData();
+        DBReader dbReader = new DBReader(this);
+        dbReader.readData();
+    }
+    
+    
+    
+    public DataContainer getDataContainer()
+    {
+    	return dataContainer;
+    }
+    
+    public SqlConnection getSqlConnection() {
+    	return sqlConnection;
     }
 }
