c909d513f47b1a16b57c03674d2d5e31cadce4e1
TYLWL <1017519980@qq.com>
2020-06-30 14:50:25 +0800

    Update Test.java

--- alc/src/main/java/com/OAT/Routing/test/Test.java
+++ alc/src/main/java/com/OAT/Routing/test/Test.java
@@ -4,7 +4,9 @@ package com.OAT.Routing.test;
 import com.OAT.Routing.DataAccess.*;
 import com.OAT.Routing.DataEntity.ORoute;
 import com.OAT.Routing.DataEntity.User;
+import com.OAT.Routing.sql.SqlConnection;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
